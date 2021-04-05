afc9cb552bff750ad355eb2ffbf9cae99a4452df
Juan Pablo Segundo <PablitoMids@Miguels-MacBook-Pro.local>
2021-04-04 23:07:41 -0500

    My second commit

---
+++ DepositoLaMaestria/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DepositoLaMaestria
+//
+//  Created by Juan Pablo Segundo on 04/04/21.
+//  Copyright © 2021 Juan Pablo Segundo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
